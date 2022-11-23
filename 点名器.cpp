3ebf39460897ee5ad9b899d3c7c967248f338c29
Find-NICK <116571277+Find-NICK@users.noreply.github.com>
2022-11-23 17:49:09 +0800

    Add files via upload

---
+++ 点名器.cpp
@@ -0,0 +1,104 @@
+//## 腾讯会议点名器 ##
+//version:1.0
+//author:Find_NICK(括号)
+//usage：参见演示视频
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include<vector>
+#include<set>
+#include<map>
+#include<ios>
+#include<iosfwd>
+#include<istream>
+#include<bitset>
+#include<cctype>
+#include<cerrno>
+#include<clocale>
+#include<complex>
+#include<cstdlib>
+#include<ctime>
+#include<fstream>
+#include<functional>
+#include<limits>
+#include<list>
+#include<ostream>
+#include<queue>
+#include<sstream>
+#include<csignal>
+#include<fstream>
+#include<functional>
+#include<windows.h>
+#define INF 0x3f3f3f3f
+#define REAL_INF 2147483647
+#define MAXN 100
+#define BUGOUT printf("Here for the No.%d BUG!\n",bug_count++)
+#define CRASH raise(SIGSEGV)
+#define CRASH_WITH_ABORT abort()
+#define CLOSE ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+#define isd(c) ('0'<=(c)&&(c)<='9')
+#define isa(c) ('a'<=(c)&&(c)<='z')
+#define isA(c) ('A'<=(c)&&(c)<='Z')
+#define endl "\n"
+#define PI acos(-1)
+using namespace std;
+int bug_count=1;
+char ID[100];
+int cnt=1;
+void init() {
+
+}
+void NUMBER(int x) {
+	BYTE key=x+0x30;
+	keybd_event(key, 0, 0 ,0);
+	Sleep(5);
+	keybd_event(key, 0, KEYEVENTF_KEYUP,0);
+	Sleep(5);
+}
+void nextid() {
+	//学号生成
+	ID[0]='0';
+	ID[1]='7';
+	ID[2]='0'+cnt/10;
+	ID[3]='0'+cnt%10;
+}
+void clr() {
+	int len=strlen(ID);
+	for(int i=0; i<len; i++) {
+		keybd_event(VK_BACK, 0, 0 ,0);
+		Sleep(1);
+		keybd_event(VK_BACK, 0, KEYEVENTF_KEYUP,0);
+		Sleep(1);
+	}
+}
+void run() {
+	int len=strlen(ID);
+	for(int i=0; i<len; i++) {
+		NUMBER(ID[i]-'0');
+	}
+}
+int main() {
+	//
+	//codes here !
+	//
+	Sleep(3000);
+	for(int i=0; i<44; i++) {
+		nextid();
+		cnt++;
+		run();	
+		Sleep(800);
+		clr();
+	}
+	return 0;
+}
+// 0123456789
+
+
+
